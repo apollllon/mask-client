7ccfcf585f281cb4a9ed07e416b6c3dae2fedb92
Apollon Nishikawa <apollon.nishikawa@irsc.jp>
2020-12-20 12:59:29 +0900

    臭気コンポーネントの表示を修正

--- src/components/SmellWidget.tsx
+++ src/components/SmellWidget.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
-import { Typography } from '@material-ui/core';
+import { Typography, Box, makeStyles } from '@material-ui/core';
 import Widget from './shared/Widget';
 
 // 笑顔
 import MoodIcon from '@material-ui/icons/Mood';
 // 嫌な顔
 import MoodBadIcon from '@material-ui/icons/MoodBad';
+import { theme } from '../materialui/theme';
+
+const useStyles = makeStyles({
+  score: {
+    marginRight: theme.spacing(1)
+  },
+  goodStateContainer: {
+    color: 'blue',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  badStateContainer: {
+    color: 'red',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  icon: {
+    fontSize: 40,
+    marginRight: theme.spacing(1)
+  }
+})
 
 type SmellWidgetProps = {
   smell: number
@@ -13,22 +34,34 @@ type SmellWidgetProps = {
 }
 
 const SmellWidget: React.FC<SmellWidgetProps> = (props) => {
+  const classes = useStyles()
+
+
+  console.log('thre', props.smellThreshold)
+  console.log('boolean', props.smell < props.smellThreshold)
+
 
   return (
     <Widget
       title="Smell"
       body={
-        <>
-          <Typography variant="h3">
+        <Box display="flex">
+          <Typography variant="h3" className={classes.score}>
             {props.smell}
           </Typography>
           {
             props.smell < props.smellThreshold ?
-              <MoodBadIcon style={{ color: 'red', fontSize: 40 }} />
+              <div className={classes.badStateContainer}>
+                <MoodBadIcon className={classes.icon} />
+                <Typography variant="h5">bad</Typography>
+              </div>
               :
-              <MoodIcon style={{ color: 'blue', fontSize: 40 }} />
+              <div className={classes.goodStateContainer}>
+                <MoodIcon className={classes.icon} />
+                <Typography variant="h5">good</Typography>
+              </div>
           }
-        </>
+        </Box>
       }
     />
   )
